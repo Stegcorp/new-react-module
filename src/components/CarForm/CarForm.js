a47c89e161415f2741513e4322ac1247201d77c5
stegcorp <stegcorp@gmail.com>
2023-04-19 21:53:13 +0300

    hw3 car validator

--- src/components/CarForm/CarForm.js
+++ src/components/CarForm/CarForm.js
@@ -1,9 +1,14 @@
 import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
 import {carService} from "../../services/cars.service";
+import {joiResolver} from '@hookform/resolvers/joi'
+import {carValidator} from "../../validators/car.validator";
 
 const CarForm = ({setAllCar, carForUpdate}) => {
-    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm();
+    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm({
+        mode: "all",
+        resolver: joiResolver(carValidator)
+    });
     useEffect(() => {
         if (carForUpdate) {
             setValue('brand', carForUpdate.brand, {shouldValidate: true})
@@ -17,7 +22,7 @@ const CarForm = ({setAllCar, carForUpdate}) => {
             await carService.updateById(carForUpdate.id, car);
             setAllCar(carForUpdate)
         } else {
-          const {data} = await carService.create(car);
+            const {data} = await carService.create(car);
             setAllCar(data)
         }
         reset()
@@ -26,8 +31,11 @@ const CarForm = ({setAllCar, carForUpdate}) => {
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder={'brand'} {...register('brand')}/>
+            {errors.brand && <span>{errors.brand.message}</span>}
             <input type="text" placeholder={'price'} {...register('price', {valueAsNumber: true})}/>
+            {errors.price && <span>{errors.price.message}</span>}
             <input type="text" placeholder={'year'} {...register('year', {valueAsNumber: true})}/>
+            {errors.year && <span>{errors.year.message}</span>}
             <button>{carForUpdate ? 'Update' : 'Save'}</button>
         </form>
     );
--- src/validators/car.validator.js
+++ src/validators/car.validator.js
@@ -1,1 +1,15 @@
+import Joi from "joi";
+
+const carValidator = Joi.object({
+    brand: Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
+        'string.pattern.base': 'Бренд має складатися тільки з літер, мін 1 макс 20 літер',
+        'string.required': 'Це поле обовя\'зкове'
+    }),
+    price: Joi.number().min(0).max(1000000).required(),
+    year: Joi.number().min(1990).max(new Date().getFullYear()).required()
+});
+
+export {
+    carValidator
+}
 
